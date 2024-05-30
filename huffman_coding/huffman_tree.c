0abc19783a38823f89c69479e1ae0d6d0e3d01e1
FrankOnyemaOrji <f.orji@alustudent.com>
2024-05-30 10:56:28 +0200

    New change to file

--- huffman_coding/huffman_tree.c
+++ huffman_coding/huffman_tree.c
@@ -1,33 +1,66 @@
-#include "heap.h"
 #include "huffman.h"
 
 /**
- * huffman_tree - A function that builds the Huffman tree
- * @data: An array of characters of size @size
- * @freq: An array containing the associated frequencies (of size @size too)
- * @size: size of an array
- * Return: A pointer to the root node of the Huffman tree, or NULL if it fails
+ * freeNestedNode - program that is used as a free_data parameter
+ * to heap_delete
+ * this function frees memory allocated for a binary_tree_node_t node
+ * containing a symbol_t struct
+ * @data: the void pointer intended to be cast into binary_tree_node_t pointer
+ * Return: nothing (void)
  */
+
+void freeNestedNode(void *data)
+{
+	binary_tree_node_t *node_data = NULL;
+	symbol_t *symbol_data = NULL;
+
+	node_data = (binary_tree_node_t *)data;
+
+	if (node_data)
+	{
+		symbol_data = (symbol_t *)(node_data->data);
+
+		if (symbol_data)
+			free(symbol_data);
+
+		free(node_data);
+	}
+}
+
+/**
+ * huffman_tree - program that constructs a Huffman tree using character data
+ * and their frequencies
+ * @data: an array of characters
+ * @freq: an array of frequencies associated with characters
+ * @size: the size of the arrays
+ * Return: a pointer to the root node of the constructed Huffman tree
+ */
+
 binary_tree_node_t *huffman_tree(char *data, size_t *freq, size_t size)
 {
 	heap_t *priority_queue = NULL;
-	binary_tree_node_t *huffman_tree = NULL;
-	int ret = 1;
+	binary_tree_node_t *huffman_root = NULL;
 
-	if (data == NULL || freq == NULL || size == 0)
+	if (!data || !freq || size == 0)
 		return (NULL);
+
 	priority_queue = huffman_priority_queue(data, freq, size);
-	if (priority_queue == NULL)
+
+	if (!priority_queue)
 		return (NULL);
 
-	while (priority_queue->size > 1 && ret)
+	while (priority_queue->root && (priority_queue->root->left ||
+					priority_queue->root->right))
 	{
-		ret = huffman_extract_and_insert(priority_queue);
+		if (!huffman_extract_and_insert(priority_queue))
+		{
+			heap_delete(priority_queue, freeNestedNode);
+			return (NULL);
+		}
 	}
-	if (ret)
-	{
-		huffman_tree = priority_queue->root->data;
-		heap_delete(priority_queue, NULL);
-	}
-		return (huffman_tree);
+
+	huffman_root = (binary_tree_node_t *)(priority_queue->root->data);
+	heap_delete(priority_queue, NULL);
+
+	return (huffman_root);
 }
